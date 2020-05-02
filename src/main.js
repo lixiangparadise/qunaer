e5a1ffec5c80acd9eaa2e3271d7d56cbf97f0e39
lixiangparadise <18862107076@163.com>
2020-05-02 19:38:56 +0800

    lunbotu

--- src/main.js
+++ src/main.js
@@ -5,6 +5,10 @@ import App from './App'
 import router from './router'
 // click事件延迟问题
 import fastClick from 'fastclick'
+//引入轮播图插件
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+// import style
+import 'swiper/css/swiper.css'
 // 引入rest.css 不同浏览器显示不同问题
 import 'styles/reset.css'
 // border.css 1px边框像素问题
@@ -12,8 +16,10 @@ import 'styles/border.css'
 // 引入iconfont
 import 'styles/iconfont.css'
 
+
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class='wrapper'>
+        <swiper :options="swiperOption">
+            <!-- slides -->
+            <swiper-slide  v-for="item of swiperList" :key="item.id">
+                <img class="swiper-img" :src="item.imgUrl"/>
+            </swiper-slide>
+            <!-- <swiper-slide>
+                <img class="swiper-img" src="https://imgs.qunarzz.com/p/tts8/1912/99/49185399009dca02.jpg_750x440_db4faff3.jpg"/>
+            </swiper-slide> -->
+            <!-- Optional controls -->
+            <div class="swiper-pagination"  slot="pagination"></div>
+        </swiper>
+    </div>
+</template>
+<script>
+export default {
+    name: 'HomeSwiper',
+    data(){
+        return{
+            //设置轮播
+            swiperOption:{
+                pagination: '.swiper-pagination',
+                loop:true //循环
+            },
+            //list图片
+            swiperList:[
+                {
+                    id:'0001',
+                    imgUrl: 'https://imgs.qunarzz.com/p/tts3/1912/9f/a68ead8501fa6b02.jpg_750x440_608aa936.jpg'
+
+                },{
+                    id:'0002',
+                    imgUrl: 'https://imgs.qunarzz.com/p/tts8/1912/99/49185399009dca02.jpg_750x440_db4faff3.jpg'
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+.wrapper >>> .swiper-pagination-bullet-active
+    background:#fff
+.wrapper
+    width:100%
+    height: 60vw
+    background-color:#eee
+    
+    .swiper-img
+        width:100%
+</style>
