f76f0352cfc8f91620577a926dd892f730813274
lixiangparadise <18862107076@163.com>
2020-05-02 22:51:02 +0800

    add recommend and weekend

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,85 @@
+<template>
+    <div>
+        <div class="title">热销推荐</div>
+        <ul>
+            <!-- border-bottom 1px边框 -->
+            <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+                <img class="item-img" :src='item.imgUrl' />
+                <div class='item-info'>
+                    <p class='item-title'>{{item.title}}</p>
+                    <p class='item-desc'>{{item.desc}}</p>
+                    <button class='item-button'>查看详情</button>
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeRecommend',
+    data(){
+        return{
+            recommendList:[
+                {
+                    id:'0001',
+                    imgUrl:'http://img1.qunarzz.com/sight/p0/201301/14/bf26ff3662c7a2a393835fbb.png_200x200_35c6466e.png',
+                    title:'苏州海洋馆',
+                    desc:'连鱼儿都是成双成对的，你还在等着谁？'
+                },
+                {
+                    id:'0002',
+                    imgUrl:'http://img1.qunarzz.com/sight/p0/1412/b4/77f601b2a965f62a39a8939fbd5db488.water.jpg_200x200_48b526c5.jpg',
+                    title:'苏七里山塘',
+                    desc:'千年的山塘街，千年的河水依然流淌。如果你到山塘街却不登一叶画舫，确为一大憾事。自古戏台登舟，安静的电瓶船将为你开创另一段思绪，穿过古老的桥洞，看着枕河人家的生活，粉墙黛瓦，斑驳的历史痕迹在你身边流淌，忘却了都市的喧嚣，无谓人生的烦忧。'
+                },
+                {
+                    id:'0003',
+                    imgUrl:'http://img1.qunarzz.com/sight/p0/1412/10/e980cba87b79a92f54c511a393749b8d.water.jpg_200x200_d8ceaa65.jpg',
+                    title:'穹窿山景区',
+                    desc:'主峰“箬帽峰”，海拔341.7米，素有“吴中之巅”之称。'
+                }
+
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl'
+    .title
+        margin-top:0.2rem
+        line-height: 0.8rem
+        background-color: #eee
+        text-indent:0.2rem
+    .item
+        overflow:hidden
+        display:flex
+        height: 1.9rem
+        // background-color red
+        .item-img
+            width:1.7rem
+            height: 1.7rem
+            padding: 0.1rem
+        .item-info
+            // background-color green
+            flex:1
+            min-width:0 //保证内容不超出外层容器
+            .item-title
+                // background-color green
+                line-height:0.54rem
+                font-size: 0.32rem
+                ellipsis() //省略号
+            .item-desc
+                line-height:0.4rem
+                color:#ccc
+                ellipsis()
+            .item-button
+                line-height:0.4rem
+                margin-top: 0.1rem
+                background: #ff9300
+                padding: 0 0.2rem
+                border-radius: 0.06rem
+                color: #fff
+</style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,74 @@
+<template>
+    <div>
+        <div class="title">周末去哪儿</div>
+        <ul>
+            <!-- border-bottom 1px边框 -->
+            <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+                <div  class='item-img-wrapper'>
+                    <img class="item-img" :src='item.imgUrl' />
+                </div>
+                <div class='item-info'>
+                    <p class='item-title'>{{item.title}}</p>
+                    <p class='item-desc'>{{item.desc}}</p>
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeWeekend',
+    data(){
+        return{
+            recommendList:[
+                {
+                    id:'0001',
+                    imgUrl:'//img1.qunarzz.com/sight/p0/1603/85/85aa0c000578a01e90.water.jpg_600x330_6dddcf68.jpg',
+                    title:'拙政园',
+                    desc:'这一大观园式的古典豪华园林，以其布局的山岛、竹坞、松岗、曲水之趣，被胜誉为“天下园林之典范”。'
+                },
+                {
+                    id:'0002',
+                    imgUrl:'//img1.qunarzz.com/sight/p0/2004/b5/b5eb5856f343534da3.water.jpg_600x330_f47eee3e.jpg',
+                    title:'华谊兄弟电影世界',
+                    desc:'作为华谊兄弟实景娱乐的旗舰项目，华谊兄弟电影世界坐落于风光旖旎的苏州阳澄湖半岛国家旅游度假区。'
+                },
+                {
+                    id:'0003',
+                    imgUrl:'//img1.qunarzz.com/sight/p0/1805/16/16b71336f2019286a3.img.jpg_600x330_b3e581bf.jpg',
+                    title:'上方山森林动物世界',
+                    desc:'上方山森林动物世界位于上方山南麓，占地面积为24042.5平方米，是一家集野生动物保护繁育、科普教育、生态游览及休闲娱乐于一体的综合性生境模拟展区。'
+                }
+
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl'
+    .title
+        margin-top:0.2rem
+        line-height: 0.8rem
+        background-color: #eee
+        text-indent:0.2rem
+    .item-img-wrapper
+        overflow:hidden
+        height:0
+        padding-bottom:55%
+    .item-img
+        width:100%
+    .item-info
+        padding :0.1rem
+        .item-title
+            // background-color green
+            line-height:0.54rem
+            font-size: 0.32rem
+            ellipsis() //省略号
+        .item-desc
+            line-height:0.4rem
+            color:#ccc
+            ellipsis()
+</style>
