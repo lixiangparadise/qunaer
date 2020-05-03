c34cbe9d51f84614532a305f9e0b20bb1b8a99ad
lixiangparadise <18862107076@163.com>
2020-05-03 14:21:41 +0800

    add city search

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class='search'>
+        <input class="search-input" type="text" placeholder="输入城市名或拼音"/>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'CitySearch'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+.search
+    height:0.72rem
+    background-color:$bgColor
+    font-size: 0.32rem
+    padding:0 0.1rem
+    .search-input
+        box-sizing: border-box
+        width:100%
+        padding: 0 0.1rem
+        height: 0.62rem
+        line-height 0.62rem
+        text-align:center
+        border-radius:0.06rem
+        color:#666
+</style>
