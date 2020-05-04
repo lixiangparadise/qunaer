5a955f81573cee36f80cee7f47d7a81e09b4e5b3
lixiangparadise <18862107076@163.com>
2020-05-04 11:33:48 +0800

    detail header animation

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,36 +1,27 @@
 <template>
     <div class='header'>
         <detail-banner></detail-banner>
+        <detail-header></detail-header>
+        <div class="content"></div>
     </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
+import DetailHeader from './components/Header'
+
+
 export default {
     name:'Detail',
     components:{
-        DetailBanner
+        DetailBanner,
+        DetailHeader
     }
 }
 </script>
 
 <style lang="stylus" scoped>
 @import '~styles/varibles.styl'
-// .header
-//     position: relative
-//     overflow hidden
-//     height:$headerHeight
-//     line-height: $headerHeight
-//     text-align: center
-//     color:#fff
-//     background-color:$bgColor
-//     font-size: 0.32rem
-//     .header-back
-//         color:#fff
-//         position:absolute
-//         top:0
-//         left:0
-//         width 0.64rem
-//         text-align:center
-//         font-size: 0.4rem
+.content
+    height:50rem
 </style>
